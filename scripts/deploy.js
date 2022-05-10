a53de5c2163d13de3bcf851805080b0d55215d02
Syed Ali Hamdani <ali.hamdani@bigimmersive.com>
2022-05-09 23:48:21 -0700

    Dynamic pricefeed of chain link addresss contract is ready and deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const DynamicPricefeed = await hre.ethers.getContractFactory("DynamicPricefeed");
+  const dynamicPricefeed= await DynamicPricefeed.deploy();
 
-  await greeter.deployed();
+  await dynamicPricefeed.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("DynamicPricefeed deployed to:",dynamicPricefeed.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/test.js
+++ test/test.js
@@ -2,28 +2,29 @@ const { expect } = require("chai");
 const { ethers,waffle } = require("hardhat");
 const provider=waffle.provider;
 
-describe("Greeter", function () {
-  beforeEach("Deploying contract for unit testing",async function(){
+describe("DynamicPricefeed", function () {
+  beforeEach("DynamicPricefeed contract for unit testing",async function(){
     [this.account1,this.account2,this.account3,this.account4,this.account5,this.account6,this.account7,this.account8,
       this.account9,this.account10,this.account11,this.account12,this.account13,this.account14,this.account15,this.account16,
       this.account17,this.account18,this.account19,this.account20]=await ethers.getSigners();
-      const Greeter = await ethers.getContractFactory("Greeter");
-    this.deploedGreeter = await Greeter.deploy("Hello, world!");
-    await this.deploedGreeter.deployed();
+      const DynamicPricefeed = await ethers.getContractFactory("DynamicPricefeed");
+    this.deploedDynamicPricefeed = await DynamicPricefeed.deploy();
+    await this.deploedDynamicPricefeed.deployed();
   })
-  it("Should return the greeting", async function () {
-    let greet=await this.deploedGreeter.greet();
-    expect(greet).to.equal("Hello, world!");
+  it("DynamicPricefeed:getPrice", async function () {
+    let getPrice=await this.deploedDynamicPricefeed.getPrice("ss");
+    // console.log(getPrice);
+    // expect(DynamicPricefeed).to.equal("Hello, world!");
   });
 
-  it("Should return the greeting", async function () {
-    await expect(this.deploedGreeter.setGreeting("no")).to.be.revertedWith("Greeter: 10 minutes has not been passed")
-  });
+  // it("Should return the greeting", async function () {
+  //   await expect(this.deploedGreeter.setGreeting("no")).to.be.revertedWith("Greeter: 10 minutes has not been passed")
+  // });
 
-  it("Should return the greeting", async function () {
-    await network.provider.send("evm_increaseTime", [600]);
-    await this.deploedGreeter.setGreeting("no");
-    let greet=await this.deploedGreeter.greet();
-    expect(greet).to.equal("no");
-  });
+  // it("Should return the greeting", async function () {
+  //   await network.provider.send("evm_increaseTime", [600]);
+  //   await this.deploedGreeter.setGreeting("no");
+  //   let greet=await this.deploedGreeter.greet();
+  //   expect(greet).to.equal("no");
+  // });
 });
